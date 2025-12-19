82186e68c7d81f0a207a39c5f9a0dc43db2d2dfe
Prince gupta <pg0799231@gmail.com>
2025-12-19 14:46:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+// Get the element with id "level"
+let element = document.getElementById("level");
+
+// Initialize level counter
+let level = 0;
+
+// Traverse up the DOM tree until html element
+while (element) {
+  level++;
+  element = element.parentElement;
+}
+
+// Show result using alert
+alert("The level of the element is: " + level);
+
